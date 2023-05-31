f56c763abee5ff2b1741da43e0e320f9a795aad5
nmmn1004 <jjj4120@gmail.com>
2023-05-31 15:27:35 +0900

    add: react 개발환경구축

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Login(){
+  <form method='post'>
+      ID
+      <input type="text" id='userID'/>
+      PW
+      <input type="text" id='userPW'/>
+  </form>
+}
+
+function App() {
+  return (
+        <Login/>
+  );
+}
+
+export default App;
